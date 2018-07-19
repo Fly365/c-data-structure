334a998e498da63d29dfc3f3d56b3a99f5af67ce
wuxingfeng <771645231@qq.com>
2018-07-19 17:05:14 +0800

    双链表

--- ch02/dlinklist.cpp
+++ ch02/dlinklist.cpp
@@ -60,5 +60,96 @@ void destoryList(DLinkNode *L){
   free(pre);
 }
 
-//链表是否为空
-bool listEmpty()
+//链表是否为空,L头结点
+bool listEmpty(DLinkNode *L){
+  return (L -> next == NULL);
+}
+
+//链表长度
+int listLength(DLinkNode *L){
+  DLinkNode *p = L;
+  int i = 0;
+  if(p -> next != NULL){
+    i++;
+    p = p -> next;
+  }
+  return i;
+}
+
+// 输出打印链表
+void  dispList(DLinkNode *L){
+  DLinkNode *p = L -> next;
+  while(p != NULL){
+    printf("%d ", p -> data);
+    p = p -> next;
+  }
+  printf("\n");
+}
+
+//获取指定索引位置的元素
+bool getElem(DLinkNode *L, int i, ElemType &e){
+  if(i < 0) return false;
+  int j = 0;
+  DLinkNode *p = L;
+  while(j < i && p != NULL){
+    j++;
+    p = p -> next;
+  }
+  if(p == NULL){
+    return false;
+  }
+  e = p -> data;
+  return true;
+}
+
+//获取和指定元素相等的第一个元素的索引位置
+int localteElem(DLinkNode *L, ElemType e){
+  int n = 1;
+  DLinkNode *p = L -> next;
+  while(p != NULL && p -> data != e){
+    n++;
+    p = p -> next;
+  }
+
+  if(p == NULL) return 0;
+  return n;
+}
+
+//链表新加元素
+bool listInsert(DLinkNode * &L, int i, ElemType e){
+  DLinkNode *p = L, *s;
+  int j = 0;
+  if(i < 0) return false;
+  while(p != NULL && j < i-1){
+    j++;
+    p = p -> next;
+  }
+  if(p == NULL) return false;
+  s = (DLinkNode *)malloc(sizeof(DLinkNode));
+  s -> data = e;
+  s -> next = p -> next;//将结点s插入到结点p之后
+  if(p -> next != NULL){
+    p -> next -> prior = s;
+  }
+  s -> prior = p;
+  p -> next = s;
+  return true;
+}
+
+//链表删除指定索引位置的元素
+bool listDelete(DLinkNode * &L, int i, ElemType &e){
+  DLinkNode *p = L -> next,*q;
+  int j = 0;
+  if(i < 0) return false;
+  if(j < i-1 && p != NULL){
+      j++;
+      p = p -> next;
+  }
+  q = p -> next;
+  if(q == NULL) return false;
+  e = p -> data;
+  p -> next = q -> next;
+  if(p -> next != NULL) p -> next -> prior = p;
+  free(q);
+  return true;
+}
